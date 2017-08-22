5d515cdcc596f124d4f61de628c1ca2a8672e760
Taras Kohut <tarasrng@gmail.com>
2017-08-23 01:26:22 +0300

    spring security dependencies

---
+++ src/main/java/org/springframework/security/samples/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package org.springframework.security.samples.config;
+
+/**
+ * Created by Taras on 22.08.2017.
+ */
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+}
